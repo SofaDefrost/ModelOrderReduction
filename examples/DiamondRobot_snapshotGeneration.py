1364497b7c1dfef1a9edb6457de6567c44a7c179
olivier-goury <olivier.goury@inria.fr>
2017-12-20 13:49:20 +0100

    Add files for examples

---
+++ examples/DiamondRobot_snapshotGeneration.py
@@ -0,0 +1,111 @@
+ 
+import Sofa
+
+import os
+path = os.path.dirname(os.path.abspath(__file__))+'/Mesh/'
+#meshRobot=path+'siliconeV0.vtu'
+#meshRobot=path+'myDiamondFairlyFine.vtu'
+meshRobot=path+'myDiamondQuiteFine.vtu'
+#meshRobot=path+'myDiamondFine.vtu'
+GREEN = "\033[1;32m "
+ENDL  = '\033[0m'
+print GREEN + "[INFO]" + ENDL + " [Scene]:" + " If the computation is slow, it might be because you did not add sparse and metis to sofa configuration."
+print GREEN + "[INFO]" + ENDL + " [Scene]:" + " (please refer to the README file for more informations)"
+
+
+def createScene(rootNode):
+
+ 	  # Root node
+#                rootNode.findData('dt').value=1
+                rootNode.findData('dt').value=0.01
+                rootNode.findData('gravity').value='0 0 -9810'
+                rootNode.createObject('VisualStyle', displayFlags='showCollision hideVisualModels showForceFields showInteractionForceFields hideCollisionModels hideBoundingCollisionModels hideWireframe')
+                rootNode.createObject('BackgroundSetting')
+		#Required plugin
+                rootNode.createObject('RequiredPlugin', pluginName='SoftRobots')
+                rootNode.createObject('RequiredPlugin', pluginName='SofaPardisoSolver')
+
+
+                rootNode.createObject('FreeMotionMasterSolver')
+		rootNode.createObject('GenericConstraintSolver', tolerance="1e-12", maxIterations="10000")
+                
+                #goal = rootNode.createChild('goal')
+                #goal.createObject('EulerImplicit', firstOrder='1')
+                #goal.createObject('CGLinearSolver', iterations='100',threshold="1e-5", tolerance="1e-5")
+                #goal.createObject('MechanicalObject', name='goalMO', position='0 0 125')
+                #goal.createObject('Sphere', radius='5', group='1')
+                #goal.createObject('UncoupledConstraintCorrection')
+
+
+                #feuille
+                feuille = rootNode.createChild('feuille')
+                feuille.createObject('EulerImplicitSolver', rayleighStiffness='0.1', rayleighMass='0.1')
+                feuille.createObject('ShewchukPCGLinearSolver', iterations="1", name="linearsolver", tolerance="1e-5", preconditioners="preconditioner", use_precond="true", update_step="1")
+                feuille.createObject('MeshVTKLoader', name="loader", filename=meshRobot) 
+                feuille.createObject('TetrahedronSetTopologyContainer', src="@loader")
+                feuille.createObject('TetrahedronSetGeometryAlgorithms', drawTetrahedra="false", template="Vec3d")
+                feuille.createObject('MechanicalObject', name="tetras", template="Vec3d", showIndices="false", showIndicesScale="4e-5", rx="90", dz="35")
+                feuille.createObject('UniformMass', totalmass="0.5")
+                #feuille.createObject('TetrahedronFEMForceField', youngModulus="450", poissonRatio="0.45")
+                poissonRatio = 0.45
+                youngModulus = 450
+                mu_ = youngModulus/(2*(1+poissonRatio))
+                lambda_ = youngModulus*poissonRatio/((1-2*poissonRatio)*(1+poissonRatio))
+                k0_ = youngModulus/(3*(1-2*poissonRatio))
+                c_1 = 250
+                c_2 = 2*mu_ - c_1
+                alpha1_ = 2
+                #feuille.createObject('TetrahedronHyperelasticityFEMForceField',materialName="StVenantKirchhoff", ParameterSet=str(mu_) + " " + str(lambda_),AnisotropyDirections="")
+                #feuille.createObject('TetrahedronHyperelasticityFEMForceField',materialName="NeoHookean", ParameterSet=str(mu_) + " " + str(k0_), AnisotropyDirections="")        	 
+                feuille.createObject('TetrahedronHyperelasticityFEMForceField',materialName="MooneyRivlin", ParameterSet=str(c_1) + " " + str(c_2) + " " + str(k0_), AnisotropyDirections="", printLog="1")    
+                #feuille.createObject('TetrahedronHyperelasticityFEMForceField',materialName="Ogden", ParameterSet=str(k0_) + " " + str(mu_) + " " + str(alpha1_), AnisotropyDirections="")        	 
+
+
+                feuille.createObject('BoxROI', name="boxROI", box="-15 -15 -40  15 15 10", drawBoxes="true")
+                feuille.createObject('FixedConstraint', indices="@boxROI.indices")
+                feuille.createObject('BoxROI', name="boxROIArms", box="-100 -100 60  100 100 70", drawBoxes="true")
+
+                feuille.createObject('SparseLDLSolver', name="preconditioner")
+                #feuille.createObject('SparsePARDISOSolver', name="preconditioner", symmetric="1")
+
+                feuille.createObject('PythonScriptController', classname="interface", filename="pythonControllers/shakeDiamondRobotNG.py")
+                #feuille.createObject('PythonScriptController', classname="measureHeight", filename="pythonControllers/measureHeight.py")
+		feuille.createObject('LinearSolverConstraintCorrection', solverName="preconditioner")
+		feuille.createObject('WriteState', filename="output/feuilleQuiteMooneyRivlin.state", period='0.02',writeX="1", writeX0="0", writeV="0")
+
+                
+                #feuille/controlledPoints
+                controlledPoints = feuille.createChild('controlledPoints')
+		controlledPoints.createObject('MechanicalObject', name="actuatedPoints", template="Vec3d", position="0 0 125   0 97 45   -97 0 45   0 -97 45  97 0 45")
+		#controlledPoints.createObject('CableConstraint', name="nord", indices="1", pullPoint="0 10 30", valueType="force", value="4000")
+		#controlledPoints.createObject('CableConstraint', name="ouest", indices="2", pullPoint="-10 0 30", valueType="force", value="4000")
+		#controlledPoints.createObject('CableConstraint', name="sud", indices="3", pullPoint="0 -10 30", valueType="force", value="4000")
+		controlledPoints.createObject('CableConstraint', name="nord", indices="1", pullPoint="0 10 30", valueType="displacement", value="0")
+		controlledPoints.createObject('CableConstraint', name="ouest", indices="2", pullPoint="-10 0 30", valueType="displacement", value="0")
+		controlledPoints.createObject('CableConstraint', name="sud", indices="3", pullPoint="0 -10 30", valueType="displacement", value="0")
+		controlledPoints.createObject('CableConstraint', name="est", indices="4", pullPoint="10 0 30")
+
+                controlledPoints.createObject('BarycentricMapping', mapForces="false", mapMasses="false")
+                
+                visuNode = feuille.createChild("visuNode")
+                visuNode.createObject("OglModel",filename=path+"surface.stl", template='ExtVec3f', color='0.7 0.7 0.7 0.6',rotation="90 0 0", translation="0 0 35")
+                visuNode.createObject("BarycentricMapping")
+
+                
+                goalTop = feuille.createChild('goalTop')
+                goalTop.createObject('MechanicalObject', name='goalMO', position='0 0 125')
+                #goalTop.createObject('WriteState', filename="output/positionGoalCOARSEfull.state", period='1',writeX="1", writeV="0")
+                #goalTop.createObject('WriteState', filename="output/positionGoalFULL.state", period='1',writeX="1", writeV="0")
+                #goalTop.createObject('WriteState', filename="output/positionGoalFairlyFULL.state", period='1',writeX="1", writeV="0")
+                goalTop.createObject('Sphere', radius='3', group='1')
+                goalTop.createObject('BarycentricMapping')
+                
+                goalArm = feuille.createChild('goalArm')
+                goalArm.createObject('MechanicalObject', name='goalMO', position='0 60 80   -60 0 80   0 -60 80   60 0 80')
+                #goalArm.createObject('WriteState', filename="output/positionArmsCOARSEfull.state", period='1',writeX="1", writeV="0")
+                #goalArm.createObject('WriteState', filename="output/positionArmsFairlyFULL.state", period='1',writeX="1", writeV="0")
+                goalArm.createObject('Sphere', radius='3', group='1')
+                goalArm.createObject('BarycentricMapping')
+                
+                return rootNode
+                
---
+++ examples/DiamondRobot_snapshotGeneration_ECSW.py
@@ -0,0 +1,155 @@
+ 
+import Sofa
+
+import os
+pathSceneFile = os.path.dirname(os.path.abspath(__file__))
+path = os.path.dirname(os.path.abspath(__file__))+'/Mesh/'
+#meshRobot=path+'siliconeV0.vtu'
+#meshRobot=path+'myDiamondFairlyFine.vtu'
+meshRobot=path+'myDiamondQuiteFine.vtu'
+#modesRobot=pathSceneFile + '/modes_Options/modes_DiamondCoarse.txt'
+#modesRobot=pathSceneFile + '/modes_Options/modes_DiamondFairlyFine.txt'
+#modesRobot=pathSceneFile + '/modes_Options/modes_DiamondFairlyFineNGaccurate.txt'
+#modesRobot=pathSceneFile + '/modes_Options/modes_DiamondQuiteFine.txt'
+#modesRobot=pathSceneFile + '/modes_Options/modesDiamondQuiteFineWellConverged.txt'
+modesRobot=pathSceneFile + '/modes_Options/modesDiamondQuiteFineWellConvergedIniRest.txt'
+
+#RIDfile = pathSceneFile + '/ECSWdata_stored/reducedIntegrationDomain_diamondWellConvergedIniRest21Modes01Tol.txt'
+#RIDfile = pathSceneFile + '/ECSWdata_stored/reducedIntegrationDomain_diamondWellConvergedIniRest21Modes003Tol.txt'
+#RIDfile = pathSceneFile + '/ECSWdata_stored/reducedIntegrationDomain_diamondWellConvergedIniRest01Tol.txt'
+#RIDfile = pathSceneFile + '/ECSWdata_stored/reducedIntegrationDomain_diamondWellConvergedIniRest005Tol.txt'
+RIDfile = pathSceneFile + '/ECSWdata_stored/reducedIntegrationDomain_diamondWellConvergedIniRest003Tol.txt'
+
+#weightsFile = pathSceneFile + '/ECSWdata_stored/weights_diamondWellConvergedIniRest21Modes01Tol.txt'
+#weightsFile = pathSceneFile + '/ECSWdata_stored/weights_diamondWellConvergedIniRest21Modes003Tol.txt'
+#weightsFile = pathSceneFile + '/ECSWdata_stored/weights_diamondWellConvergedIniRest01Tol.txt'
+#weightsFile = pathSceneFile + '/ECSWdata_stored/weights_diamondWellConvergedIniRest005Tol.txt'
+weightsFile = pathSceneFile + '/ECSWdata_stored/weights_diamondWellConvergedIniRest003Tol.txt'
+
+#listActiveNodesFile = pathSceneFile + '/ECSWdata_stored/listActiveNodes_diamondWellConvergedIniRest21Modes01Tol.txt'
+#listActiveNodesFile = pathSceneFile + '/ECSWdata_stored/listActiveNodes_diamondWellConvergedIniRest21Modes003Tol.txt'
+#listActiveNodesFile = pathSceneFile + '/ECSWdata_stored/listActiveNodes_diamondWellConvergedIniRest01Tol.txt'
+#listActiveNodesFile = pathSceneFile + '/ECSWdata_stored/listActiveNodes_diamondWellConvergedIniRest005Tol.txt'
+listActiveNodesFile = pathSceneFile + '/ECSWdata_stored/listActiveNodes_diamondWellConvergedIniRest003Tol.txt'
+
+
+#nbModes = 21
+nbModes = 34
+modesPositionStr = '0'
+for i in range(1,nbModes):
+    modesPositionStr = modesPositionStr + ' 0'
+    
+prepareECSWBool = "false"
+
+performECSWBool = "false"
+performECSWBoolMappedMatrix = "false"
+
+
+def createScene(rootNode):
+
+ 	  # Root node
+#                rootNode.findData('dt').value=1
+                rootNode.findData('dt').value=0.01
+
+                rootNode.findData('gravity').value='0 0 -9810'
+                rootNode.createObject('VisualStyle', displayFlags='showCollision showVisualModels showForceFields showInteractionForceFields hideCollisionModels hideBoundingCollisionModels hideWireframe')
+ 
+		#Required plugin
+                rootNode.createObject('RequiredPlugin', pluginName='SoftRobots')
+                rootNode.createObject('RequiredPlugin', pluginName='ModelOrderReduction')
+                
+                rootNode.createObject('FreeMotionAnimationLoop')
+		rootNode.createObject('GenericConstraintSolver', tolerance="1e-5", maxIterations="100")
+   		#rootNode.createObject('QPInverseProblemSolver', name="QP", printLog='0')
+
+
+                #goal.createObject('UncoupledConstraintCorrection')
+
+
+		solverNode = rootNode.createChild('solverNode')
+		solverNode.createObject('EulerImplicitSolver', rayleighStiffness='0.1', rayleighMass='0.1')
+                solverNode.createObject('SparseLDLSolver', name="ldlsolveur")
+	        solverNode.createObject('GenericConstraintCorrection', solverName='ldlsolveur')
+
+
+                #feuille
+		feuilleMOR = solverNode.createChild('feuilleMOR')
+
+		feuilleMOR.createObject('MechanicalObject', template='Vec1d',name='alpha', position=modesPositionStr)
+		
+		feuille = feuilleMOR.createChild('feuille')
+                feuille.createObject('MeshVTKLoader', name="loader", filename=meshRobot) 
+		feuille.createObject('Mesh',name='meshInput', src="@loader")
+                feuille.createObject('MechanicalObject', name="tetras", template="Vec3d", showIndices="false", showIndicesScale="4e-5", rx="90", dz="35")
+		feuille.createObject('ModelOrderReductionMapping', input='@../alpha', output='@./tetras',modesPath=modesRobot,listActiveNodesPath="ECSWdata_stored/listActiveNodes_Diamond.txt",performECSW="false",printLog="0")
+                feuille.createObject('UniformMass', name="diamondMass",totalmass="0.5")
+                #feuille.createObject('TetrahedronFEMForceField', youngModulus="450", poissonRatio="0.45")
+		#feuille.createObject('HyperReducedTetrahedronFEMForceField', youngModulus="450", poissonRatio="0.45", name='DiamondWellConverged_HyperReducedFF_Quite_'+ str(nbModes), src="@meshInput", prepareECSW=prepareECSWBool, performECSW=performECSWBool, nbModes=str(nbModes), modesPath=modesRobot, RIDPath=RIDfile, weightsPath=weightsFile,nbTrainingSet="135", periodSaveGIE="10",printLog="0") 
+		poissonRatio = 0.45
+                youngModulus = 450
+                mu_ = youngModulus/(2*(1+poissonRatio))
+                lambda_ = youngModulus*poissonRatio/((1-2*poissonRatio)*(1+poissonRatio))
+                k0_ = youngModulus/(3*(1-2*poissonRatio))
+                c_1 = 250
+                c_2 = 2*mu_ - c_1
+                alpha1_ = 2
+                #feuille.createObject('TetrahedronHyperelasticityFEMForceField',materialName="StVenantKirchhoff", ParameterSet=str(mu_) + " " + str(lambda_),AnisotropyDirections="")
+                #feuille.createObject('TetrahedronHyperelasticityFEMForceField',materialName="NeoHookean", ParameterSet=str(mu_) + " " + str(k0_), AnisotropyDirections="")        	 
+                feuille.createObject('TetrahedronHyperelasticityFEMForceField',name='DiamondWellConverged_HyperElasticHyperReducedFF_Quite_'+ str(nbModes), materialName="MooneyRivlin", ParameterSet=str(c_1) + " " + str(c_2) + " " + str(k0_), AnisotropyDirections="", printLog="1")    
+
+		
+                feuille.createObject('BoxROI', name="boxROI", box="-15 -15 -40  15 15 10", drawBoxes="true")
+                feuille.createObject('PythonScriptController', classname="interface", filename="pythonControllers/shakeDiamondRobotNG.py")
+#                feuille.createObject('FixedConstraint', indices="@boxROI.indices")
+                #feuille.createObject('WriteState', filename="output/DiamondQuiteECSWcheckMate.state", period='1',writeX="1", writeV="0")
+                solverNode.createObject('MappedMatrixForceFieldAndMass', template='Vec1d,Vec1d', object1='@./feuilleMOR/alpha', object2='@./feuilleMOR/alpha', mappedForceField='@./feuilleMOR/feuille/DiamondWellConverged_HyperElasticHyperReducedFF_Quite_'+ str(nbModes) ,  mappedMass='@./feuilleMOR/feuille/diamondMass', performECSW=performECSWBoolMappedMatrix, listActiveNodesPath=listActiveNodesFile, timeInvariantMapping = 'true', saveReducedMass="false", usePrecomputedMass="true", precomputedMassPath='ECSWdata_stored/diamondMass_reduced34modes.txt', printLog="0")
+                #feuille/controlledPoints
+                controlledPoints = feuille.createChild('controlledPoints')
+		controlledPoints.findData('printLog').value=1
+		controlledPoints.createObject('MechanicalObject', name="actuatedPoints", template="Vec3d", position="0 97 45   -97 0 45   0 -97 45  97 0 45")
+		controlledPoints.createObject('CableConstraint', name="nord", indices="0", pullPoint="0 10 30")
+		controlledPoints.createObject('CableConstraint', name="ouest", indices="1", pullPoint="-10 0 30")
+		controlledPoints.createObject('CableConstraint', name="sud", indices="2", pullPoint="0 -10 30")
+		#controlledPoints.createObject('CableConstraint', name="nord", indices="0", pullPoint="0 10 30", valueType="displacement", value="40")
+		#controlledPoints.createObject('CableConstraint', name="ouest", indices="1", pullPoint="-10 0 30", valueType="displacement", value="40")
+		#controlledPoints.createObject('CableConstraint', name="sud", indices="2", pullPoint="0 -10 30", valueType="displacement", value="40")
+		controlledPoints.createObject('CableConstraint', name="est", indices="3", pullPoint="10 0 30")
+
+                controlledPoints.createObject('BarycentricMapping', mapForces="false", mapMasses="false")
+                
+                if (performECSWBool == 'true'):
+                    outputGoalName = "positionGoalECSW"
+                    outputArmsName = "positionArmsECSW"
+                else:
+                    outputGoalName = "positionGoalPOD"
+                    outputArmsName = "positionArmsPOD"
+
+                    
+                goalTop = feuille.createChild('goalTop')
+                goalTop.createObject('MechanicalObject', name='goalMO', position='0 0 125')
+                #goalTop.createObject('WriteState', filename="output/" + outputGoalName + ".state", period='1',writeX="1", writeV="0")
+                goalTop.createObject('Sphere', radius='3', group='1')
+                goalTop.createObject('BarycentricMapping')
+                
+                goalArm = feuille.createChild('goalArm')
+                goalArm.createObject('MechanicalObject', name='goalMO', position='0 60 80   -60 0 80   0 -60 80   60 0 80')
+                #goalArm.createObject('WriteState', filename="output/" + outputArmsName + ".state", period='1',writeX="1", writeV="0")
+                goalArm.createObject('Sphere', radius='3', group='1')
+                goalArm.createObject('BarycentricMapping')
+
+
+                visuNode = feuille.createChild("visuNode")
+                visuNode.createObject("OglModel",filename=path+"surface.stl", template='ExtVec3f', color='0.7 0.7 0.7 0.6',rotation="90 0 0", translation="0 0 35")
+                visuNode.createObject("BarycentricMapping")
+                
+                #modelCollis = feuille.createChild('modelCollis')
+		#modelCollis.createObject('MeshSTLLoader', name='loader', filename=path+"surface.stl",rotation="90 0 0", translation="0 0 35")
+		#modelCollis.createObject('TriangleSetTopologyContainer', src='@loader', name='container')
+		#modelCollis.createObject('MechanicalObject', name='collisMO', template='Vec3d')
+		#modelCollis.createObject('Triangle',group="0")
+		#modelCollis.createObject('Line',group="0")
+		#modelCollis.createObject('Point',group="0")
+		#modelCollis.createObject('BarycentricMapping')
+
+                return rootNode
+                
