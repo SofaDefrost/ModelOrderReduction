be0d07468f99530eadab651766c67b1f514a0732
VannesteFelix <felix.vanneste@isen.yncrea.fr>
2018-07-23 14:36:42 +0200

    [MOR] Update cmake forcefield with new component name

--- src/component/forcefield/Forcefield_Files.cmake
+++ src/component/forcefield/Forcefield_Files.cmake
@@ -10,14 +10,10 @@ list(APPEND HEADER_FILES
     src/component/forcefield/HyperReducedTetrahedronHyperelasticityFEMForceField.inl
     src/component/forcefield/HyperReducedTriangleFEMForceField.h
     src/component/forcefield/HyperReducedTriangleFEMForceField.inl
-    src/component/forcefield/MappedMatrixForceFieldAndMass.h
-    src/component/forcefield/MappedMatrixForceFieldAndMass.inl
-    src/component/forcefield/MappedMatrixForceFieldAndMassMOR.h
-    src/component/forcefield/MappedMatrixForceFieldAndMassMOR.inl
+    src/component/forcefield/MechanicalMatrixMapperMOR.h
+    src/component/forcefield/MechanicalMatrixMapperMOR.inl
     src/component/forcefield/MooneyRivlin.h
-    src/component/forcefield/NeoHookean.h
-
-    )
+    src/component/forcefield/NeoHookean.h)
 
 list(APPEND SOURCE_FILES
 
@@ -25,7 +21,4 @@ list(APPEND SOURCE_FILES
     src/component/forcefield/HyperReducedTetrahedronFEMForceField.cpp
     src/component/forcefield/HyperReducedTetrahedronHyperelasticityFEMForceField.cpp
     src/component/forcefield/HyperReducedTriangleFEMForceField.cpp
-    src/component/forcefield/MappedMatrixForceFieldAndMass.cpp
-    src/component/forcefield/MappedMatrixForceFieldAndMassMOR.cpp
-
-    )
+    src/component/forcefield/MechanicalMatrixMapperMOR.cpp)
