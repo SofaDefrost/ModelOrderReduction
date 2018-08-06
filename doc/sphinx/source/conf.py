57ccd3ad95cd8b119f3d43ebbc6ae0dde7caa0ac
VannesteFelix <felix.vanneste@isen.yncrea.fr>
2018-08-06 17:20:07 +0200

    [MOR] Fix sphinx conf to build doxygen doc on readthedocs with breathe

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -14,7 +14,20 @@
 #
 import os
 import sys
+
 sys.path.append(os.getcwd()+"/../../../python")
+
+
+# -- To Build Breath doc with ReadTheDocs--------------------------------------
+
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
+
 
 # -- Project information -----------------------------------------------------
 
