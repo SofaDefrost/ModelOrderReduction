5bfe3b4e05a5e74a472e3d8ce2d910f4466906af
alxbilger <alxbilger@users.noreply.github.com>
2021-07-26 10:18:47 +0200

    Remove addSubKToMatrix according to latest changes in SOFA

--- src/component/forcefield/HyperReducedRestShapeSpringsForceField.h
+++ src/component/forcefield/HyperReducedRestShapeSpringsForceField.h
@@ -119,8 +119,6 @@ public:
     /// Brings ForceField contribution to the global system stiffness matrix.
     virtual void addKToMatrix(const core::MechanicalParams* mparams, const sofa::core::behavior::MultiMatrixAccessor* matrix ) override;
 
-    virtual void addSubKToMatrix(const core::MechanicalParams* mparams, const sofa::core::behavior::MultiMatrixAccessor* matrix, const type::vector<unsigned> & addSubIndex ) override;
-
     virtual void draw(const core::visual::VisualParams* vparams) override;
 
     const DataVecCoord* getExtPosition() const;
