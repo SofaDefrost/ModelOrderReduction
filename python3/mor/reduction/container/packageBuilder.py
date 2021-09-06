ed1fa39a37351a204a5c8ea9b1ae3238e261fe5a
samFabrice <dsamfabrice5@gmail.com>
2021-09-06 13:17:51 +0200

    Updated the quadruped.py scene

--- python3/mor/reduction/container/packageBuilder.py
+++ python3/mor/reduction/container/packageBuilder.py
@@ -148,5 +148,5 @@ class PackageBuilder():
                 else : print(line),
 
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print ("Unexpected error:", sys.exc_info()[0])
             raise
