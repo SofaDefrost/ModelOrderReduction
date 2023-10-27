9c6c758696319b674f086d665dfcf8e91105f103
VannesteFelix <vanneste.felix@gmail.com>
2023-10-27 16:58:54 +0200

    rm unnecessary

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -160,15 +160,15 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-html_css_files = [
-    'css/custom.css',
-]
-
-html_js_files = [
-    'js/custom.js',
-]
+# html_static_path = ['_static']
+#
+# html_css_files = [
+#     'css/custom.css',
+# ]
+#
+# html_js_files = [
+#     'js/custom.js',
+# ]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
