3d46b16b9cfe06b778069afb3dc248bcab5f1428
Alex Bilger <alxbilger@users.noreply.github.com>
2024-03-28 10:10:17 +0100

    Put hyper reduction Data in a group (#102)

--- src/ModelOrderReduction/component/forcefield/HyperReducedHelper.cpp
+++ src/ModelOrderReduction/component/forcefield/HyperReducedHelper.cpp
@@ -29,6 +29,16 @@ HyperReducedHelper::HyperReducedHelper()
 , d_RIDPath(initData(&d_RIDPath,std::string("reducedIntegrationDomain.txt"),"RIDPath","Path to the Reduced Integration domain when performing the ECSW method"))
 , d_weightsPath(initData(&d_weightsPath,std::string("weights.txt"),"weightsPath","Path to the weights when performing the ECSW method"))
 {
+    static const std::string groupName { "HyperReduction" };
+    d_prepareECSW.setGroup(groupName);
+    d_nbModes.setGroup(groupName);
+    d_modesPath.setGroup(groupName);
+    d_nbTrainingSet.setGroup(groupName);
+    d_periodSaveGIE.setGroup(groupName);
+    d_performECSW.setGroup(groupName);
+    d_RIDPath.setGroup(groupName);
+    d_weightsPath.setGroup(groupName);
+
 }
 
 void HyperReducedHelper::initMOR(unsigned nbElements, bool printLog)
