da23f8319de4cb1c6f14ec2eedd17176340d1b0f
Olivier Goury <olivier.goury@inria.fr>
2024-07-24 16:58:34 +0200

    Add reduced version of the bouncing ball

--- examples/bouncingBall/softSphereFalling_reduced.py
+++ examples/bouncingBall/softSphereFalling_reduced.py
@@ -3,9 +3,31 @@ import Sofa.Core
 import os
 pathSceneFile = os.path.dirname(os.path.abspath(__file__))
 pathMesh = os.path.dirname(os.path.abspath(__file__))+'/'
+nbrOfModes = 15
 # Units: mm, kg, s.     Pressure in kPa = k (kg/(m.s^2)) = k (g/(mm.s^2) =  kg/(mm.s^2)
+plugins=["SofaPython3","SoftRobots","ModelOrderReduction","STLIB",
+         # normally plugin Sofa.Component is enough but still warning
+         "Sofa.Component.Visual",
+         "Sofa.Component.AnimationLoop",
+         "Sofa.GL.Component.Rendering3D",
+         "Sofa.Component.Constraint.Lagrangian.Solver",
+         'Sofa.Component.IO.Mesh',
+         'Sofa.Component.Playback',
+         'Sofa.Component.Constraint.Lagrangian.Correction', # Needed to use components [GenericConstraintCorrection]
+         'Sofa.Component.Engine.Select', # Needed to use components [BoxROI]
+         'Sofa.Component.LinearSolver.Direct', # Needed to use components [SparseLDLSolver]
+         'Sofa.Component.Mapping.Linear', # Needed to use components [BarycentricMapping]
+         'Sofa.Component.Mass', # Needed to use components [UniformMass]
+         'Sofa.Component.ODESolver.Backward', # Needed to use components [EulerImplicitSolver]
+         'Sofa.Component.SolidMechanics.FEM.Elastic', # Needed to use components [TetrahedronFEMForceField]
+         'Sofa.Component.SolidMechanics.Spring', # Needed to use components [RestShapeSpringsForceField]
+         'Sofa.Component.StateContainer', # Needed to use components [MechanicalObject]
+         'Sofa.Component.Topology.Container.Dynamic'] # Needed to use components [TetrahedronSetTopologyContainer]
 
 def createScene(rootNode):
+
+                rootNode.addObject('RequiredPlugin', pluginName=plugins, printLog=False)
+
                 rootNode.addObject('VisualStyle', displayFlags='showVisualModels hideBehaviorModels showCollisionModels hideBoundingCollisionModels showForceFields showInteractionForceFields hideWireframe')
                 rootNode.gravity=[0,-9810, 0]
                 rootNode.dt = 0.0001
@@ -27,14 +49,21 @@ def createScene(rootNode):
                 ##########################################
                 
                 sphereTranslation = [0,10,0]
-                model = solverNode.addChild('model')
+                modesNode = solverNode.addChild('modesNode')
+                modesNode.addObject('MechanicalObject', position = [0]*nbrOfModes, name='modes', template='Vec1d', showIndices='false')
+
+                model = modesNode.addChild('model')
                 model.addObject('MeshVTKLoader', name='loader', filename=pathMesh+'sphere.vtk', translation=sphereTranslation)
                 model.addObject('Mesh',src = '@loader')
                 model.addObject('MechanicalObject', name='tetras', template='Vec3d', showIndices='false', showIndicesScale='4e-5', rx='0',printLog="0")
 #                model.addObject('WriteState', name="StateWriter", filename="fullBall.state",period="0.001", writeX=True, writeX0=True, writeV=False, writeF=False, time=0)
                 model.addObject('UniformMass', totalMass='0.2', printLog='0')
-                model.addObject('TetrahedronFEMForceField', template='Vec3d',youngModulus=1.0e3,poissonRatio=0.45)
-                                
+#                model.addObject('TetrahedronFEMForceField', template='Vec3d',youngModulus=1.0e3,poissonRatio=0.45)
+#                model.addObject('HyperReducedTetrahedronFEMForceField' , template = 'Vec3', method = 'large', name = 'reducedFF_modelNode_0', poissonRatio = 0.45, youngModulus = 1.0e3, nbModes = nbrOfModes, prepareECSW = True, performECSW = False, modesPath = 'modes.txt',nbTrainingSet=100,periodSaveGIE=10)
+                model.addObject('HyperReducedTetrahedronFEMForceField' , template = 'Vec3', method = 'large', name = 'reducedFF_modelNode_0', poissonRatio = 0.45, youngModulus = 1.0e3, nbModes = nbrOfModes, prepareECSW = False, performECSW = True, modesPath = 'modes.txt', RIDPath='RID.txt',weightsPath='weights.txt')
+
+                model.addObject('ModelOrderReductionMapping' , input = '@../modes', modesPath = 'modes.txt', output = '@./tetras')
+
                 modelCollis = model.addChild('modelCollis')
                 modelCollis.addObject('MeshSTLLoader', name='loader', filename=pathMesh+'sphere.stl', rotation="0 0 0", translation=sphereTranslation)
                 #modelCollis.addObject('MeshSTLLoader', name='loader', filename=pathMesh+'sphereFine.stl', rotation="0 0 0", translation=sphereTranslation)
